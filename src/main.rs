a87114700512aab2e2e6a0a47b415ef8db9caea8
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-06 23:38:41 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,42 @@ mod stmt;
 mod utils;
 mod value;
 
-use block::*;
-use expr::*;
-use lexer::*;
-use oper::*;
-use stmt::*;
-use utils::*;
-use value::*;
+use clap::Parser;
+use std::{
+    fs::{File, read_to_string},
+    io::Write,
+    path::Path,
+};
+use {block::*, expr::*, lexer::*, oper::*, stmt::*, utils::*, value::*};
+
+#[derive(Parser)]
+#[command(
+    name = "mystia",
+    about = "Web frontend programming language outputs wasm"
+)]
+struct Cli {
+    /// Source code file path
+    path: String,
+}
 
 fn main() {
     let mut compiler = Compiler::new();
-    println!(
-        "{}",
-        compiler
-            .build("fn fact(n) { if n == 0 then 1 else fact(n-1) * n }; fact(5)")
-            .unwrap()
-    );
+    let cli = Cli::parse();
+    if let Ok(source) = read_to_string(cli.path.clone()) {
+        if let Some(wat_code) = compiler.build(&source) {
+            if let Ok(mut output_file) = File::create(Path::new(&cli.path).with_extension("wat")) {
+                if output_file.write_all(wat_code.as_bytes()).is_err() {
+                    eprintln!("Failed to write output in the file")
+                }
+            } else {
+                eprintln!("Failed to create output file")
+            }
+        } else {
+            eprintln!("Failed to compile Mystia code")
+        }
+    } else {
+        eprintln!("Failed to read source file")
+    }
 }
 
 struct Compiler {
