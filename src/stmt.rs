f449766b3f8d6bbd376eb14bbc811e7b7ce03d1f
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-06 21:04:44 +0900

    Create stmt.rs

---
+++ src/stmt.rs
@@ -0,0 +1,41 @@
+use crate::*;
+
+pub enum Stmt {
+    Defun {
+        name: String,
+        args: Vec<String>,
+        body: Expr,
+    },
+    Expr(Expr),
+}
+
+impl Stmt {
+    pub fn parse(source: &str) -> Option<Self> {
+        if let Some(source) = source.strip_prefix("fn ") {
+            let (name, source) = source.split_once("(")?;
+            let (args, body) = source.split_once(")")?;
+            Some(Stmt::Defun {
+                name: name.trim(),
+                args: args.split(",").map(|x| x.trim()).collect(),
+                body: Expr::parse(body)?,
+            })
+        } else {
+            Some(Stmt::Expr(Expr::parse(source)?))
+        }
+    }
+
+    pub fn compile(&self, ctx: &mut Compiler) -> String {
+        match self {
+            Stmt::Expr(expr) => expr.compile(),
+            Stmt::Defun { name, args, body } => format!(
+                "(func ${name} {} (result i32) {})",
+                join!(
+                    args.iter()
+                        .map(|x| format!("(param ${x} i32)"))
+                        .collect::<Vec<_>>()
+                ),
+                body.compile()
+            ),
+        }
+    }
+}
