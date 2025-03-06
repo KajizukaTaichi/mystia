a5e45f4d6fb18e6561451132373abcb9e8605efd
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-06 21:04:43 +0900

    Create stmt.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 mod expr;
 mod lexer;
 mod oper;
+mod stmt;
 mod utils;
 mod value;
 
 use expr::*;
 use lexer::*;
 use oper::*;
+use stmt::*;
 use utils::*;
 use value::*;
 
 fn main() {
     println!(
         r#"(module (func (export "_start") (result i32) {}))"#,
-        Expr::parse("1+2*3-10").map(|x| x.compile()).unwrap()
+        Stmt::parse("1+2*3-10")
+            .map(|x| x.compile(&mut Compiler { declare: vec![] }))
+            .unwrap()
     );
+}
+
+struct Compiler {
+    declare: Vec<String>,
 }
