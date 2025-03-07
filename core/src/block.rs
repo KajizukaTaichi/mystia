f50ffb4b6145cb6b46316d8a7505c44219804664
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-07 20:08:20 +0900

    Create block.rs

---
+++ core/src/block.rs
@@ -0,0 +1,18 @@
+use crate::*;
+
+#[derive(Clone, Debug)]
+pub struct Block(Vec<Stmt>);
+
+impl Block {
+    pub fn parse(source: &str) -> Option<Block> {
+        let mut result = vec![];
+        for line in tokenize(source, &[";"], false)? {
+            result.push(Stmt::parse(&line)?);
+        }
+        Some(Block(result))
+    }
+
+    pub fn compile(&self, ctx: &mut Compiler) -> String {
+        join!(self.0.iter().map(|x| x.compile(ctx)).collect::<Vec<_>>())
+    }
+}
