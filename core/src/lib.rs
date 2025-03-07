bd5c2e915917e5933ea1b6e84b87a378b5fcee1a
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-07 20:08:23 +0900

    Create lib.rs

---
+++ core/src/lib.rs
@@ -0,0 +1,27 @@
+mod block;
+mod expr;
+mod lexer;
+mod oper;
+mod stmt;
+mod utils;
+mod value;
+
+use {block::*, expr::*, lexer::*, oper::*, stmt::*, utils::*, value::*};
+
+pub struct Compiler {
+    declare: Vec<String>,
+}
+
+impl Compiler {
+    pub fn new() -> Self {
+        Compiler { declare: vec![] }
+    }
+
+    pub fn build(&mut self, source: &str) -> Option<String> {
+        Some(format!(
+            r#"(module {1} (func (export "_start") (result i32) {0}))"#,
+            Block::parse(source).map(|x| x.compile(self))?,
+            join!(self.declare)
+        ))
+    }
+}
