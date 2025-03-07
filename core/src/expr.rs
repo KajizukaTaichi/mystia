20a5917ff1819d8c9e2d897d68786599e2e9c4cf
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-07 20:08:21 +0900

    Create expr.rs

---
+++ core/src/expr.rs
@@ -0,0 +1,64 @@
+use crate::*;
+
+#[derive(Debug, Clone)]
+pub enum Expr {
+    Value(Value),
+    Ref(String),
+    Oper(Box<Oper>),
+    Call(String, Vec<Expr>),
+    Block(Block),
+}
+
+impl Expr {
+    pub fn parse(source: &str) -> Option<Expr> {
+        let source = source.trim();
+        let token_list: Vec<String> = tokenize(source.trim(), SPACE.as_ref(), true)?;
+        if token_list.len() >= 2 {
+            Some(Expr::Oper(Box::new(Oper::parse(source)?)))
+        } else {
+            let token = token_list.last()?.trim().to_string();
+            Some(
+                // Integer literal
+                if let Ok(n) = token.parse::<i32>() {
+                    Expr::Value(Value::Integer(n))
+                // Code block
+                } else if token.starts_with("{") && token.ends_with("}") {
+                    let token = token.get(1..token.len() - 1)?.trim();
+                    Expr::Block(Block::parse(token)?)
+                // prioritize higher than others
+                } else if token.starts_with("(") && token.ends_with(")") {
+                    let token = token.get(1..token.len() - 1)?.trim();
+                    Expr::parse(token)?
+                // Function call
+                } else if token.contains("(") && token.ends_with(")") {
+                    let token = token.get(..token.len() - 1)?.trim();
+                    let (name, args) = token.split_once("(")?;
+                    let args = {
+                        let mut result = vec![];
+                        for i in tokenize(args, &[","], false)? {
+                            result.push(Expr::parse(&i)?)
+                        }
+                        result
+                    };
+                    Expr::Call(name.to_string(), args)
+                // Variable reference
+                } else {
+                    Expr::Ref(token)
+                },
+            )
+        }
+    }
+
+    pub fn compile(&self, ctx: &mut Compiler) -> String {
+        match self {
+            Expr::Oper(oper) => oper.compile(ctx),
+            Expr::Ref(to) => format!("(local.get ${to})"),
+            Expr::Value(Value::Integer(n)) => format!("(i32.const {n})"),
+            Expr::Call(name, args) => format!(
+                "(call ${name} {})",
+                join!(args.iter().map(|x| x.compile(ctx)).collect::<Vec<_>>())
+            ),
+            Expr::Block(block) => block.compile(ctx),
+        }
+    }
+}
