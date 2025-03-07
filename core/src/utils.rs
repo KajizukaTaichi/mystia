e0ba2d3bab40ab06260e6f4f3176e8082fd32399
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-07 20:08:25 +0900

    Create utils.rs

---
+++ core/src/utils.rs
@@ -0,0 +1,19 @@
+pub const SPACE: [&str; 5] = [" ", "　", "\n", "\t", "\r"];
+pub const OPERATOR: [&str; 15] = [
+    "+", "-", "*", "/", "%", "^", "==", "!=", "<", ">", "<=", ">=", "&&", "||", "!",
+];
+
+pub fn include_letter(query: &str, chars: &Vec<String>, idx: usize) -> bool {
+    chars
+        .clone()
+        .get(idx..idx + query.chars().count())
+        .map(|i| query == i.concat())
+        .unwrap_or(false)
+}
+
+#[macro_export]
+macro_rules! join {
+    ($x:expr) => {
+        $x.join(&SPACE[0].to_string())
+    };
+}
