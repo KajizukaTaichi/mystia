a9027f2118d17b21ce10fdc671aadd19085503c2
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-08 14:02:48 +0900

    Update value.rs

--- core/src/value.rs
+++ core/src/value.rs
@@ -1,5 +1,35 @@
+use crate::*;
+
 #[derive(Debug, Clone)]
 pub enum Value {
     Integer(i32),
     Float(f64),
+}
+
+#[derive(Clone, Debug)]
+pub enum Type {
+    Integer,
+    Float,
+}
+
+impl Node for Type {
+    fn parse(source: &str) -> Option<Self> {
+        match source.trim() {
+            "int" => Some(Self::Integer),
+            "float" => Some(Self::Float),
+            _ => None,
+        }
+    }
+
+    fn compile(&self, _: &mut Compiler) -> String {
+        match self {
+            Self::Integer => "i32",
+            Self::Float => "f64",
+        }
+        .to_string()
+    }
+
+    fn type_infer(&self, _: &mut Compiler) -> Type {
+        self.clone()
+    }
 }
