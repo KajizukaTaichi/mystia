92f7f58fb89e5245b99e1e9694001060ad06712f
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-08 13:43:25 +0900

    Update node.rs

--- core/src/node.rs
+++ core/src/node.rs
@@ -2,6 +2,7 @@ use crate::*;
 
 pub trait Node {
     fn compile(&self, ctx: &mut Compiler) -> String;
+    fn type_infer(&self, ctx: &mut Compiler) -> Type;
     fn parse(source: &str) -> Option<Self>
     where
         Self: Node + Sized;
