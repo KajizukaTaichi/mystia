17c4f18ebf9a355d961c38cc2df0502ca21cd546
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-10 23:04:12 +0900

    Delete lib.rs

--- wasm/src/lib.rs
+++
@@ -1,7 +0,0 @@
-use mystia_core::Compiler;
-use wasm_bindgen::prelude::wasm_bindgen;
-
-#[wasm_bindgen]
-pub fn mystia_compile(source: &str) -> Option<String> {
-    Compiler::new().build(source)
-}
