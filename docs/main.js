8dbff91dec089da0d161c239ebd43c4e3a456ba9
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-10 22:34:31 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,24 +1,24 @@
-import init, { Mystia } from "./pkg/mystia_wasm";
+import init, { mystia_compile } from "./pkg/mystia_wasm.js";
 
 export async function mystia(mystiaCode) {
-    const watCode = Mystia.compile(mystiaCode);
-
-    // WATコードをバイナリにコンパイル
+    await init();
+    const watCode = mystia_compile(mystiaCode);
     const binary = await WebAssembly.compile(
         await (
             await fetch(`data:application/wasm;base64,${btoa(watCode)}`)
         ).arrayBuffer(),
     );
 
-    // WebAssemblyインスタンスを作成
     const instance = await WebAssembly.instantiate(binary);
-
-    // _start関数を呼び出す
     if (instance.exports._start) {
-        const result = instance.exports._start();
-        console.log("_start function executed.");
-        return result;
+        return instance.exports._start();
     } else {
-        console.log("_start function not found.");
+        console.log('entry point "_start" function not found.');
     }
 }
+
+console.log(
+    await (async function () {
+        await mystia("1+2");
+    })(),
+);
