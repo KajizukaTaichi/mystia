7a6f421a9272e4db33adf50ffc168237bc5828bd
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-19 20:43:02 +0900

    Delete node.rs

--- core/src/node.rs
+++
@@ -1,9 +0,0 @@
-use crate::*;
-
-pub trait Node {
-    fn compile(&self, ctx: &mut Compiler) -> Option<String>;
-    fn type_infer(&self, ctx: &mut Compiler) -> Option<Type>;
-    fn parse(source: &str) -> Option<Self>
-    where
-        Self: Node + Sized;
-}
