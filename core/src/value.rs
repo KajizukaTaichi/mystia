6b8ff2d643407a334a74752d71be4cf2dd9c5965
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-20 22:31:35 +0900

    Update value.rs

--- core/src/value.rs
+++ core/src/value.rs
@@ -8,6 +8,51 @@ pub enum Value {
     String(String),
 }
 
+impl Node for Value {
+    fn parse(source: &str) -> Option<Self> {
+        Some(
+            // Integer literal
+            if let Ok(n) = source.parse::<i32>() {
+                Value::Integer(n)
+            // Float number literal
+            } else if let Ok(n) = source.parse::<f64>() {
+                Value::Float(n)
+            // Boolean literal
+            } else if let Ok(n) = source.parse::<bool>() {
+                Value::Bool(n)
+            } else if source.starts_with("\"") && source.ends_with("\"") {
+                let source = source.get(1..source.len() - 1)?.trim();
+                Value::String(str_escape(source))
+            } else {
+                return None;
+            },
+        )
+    }
+
+    fn compile(&self, ctx: &mut Compiler) -> Option<String> {
+        Some(match self {
+            Value::Integer(n) => format!("(i32.const {n})"),
+            Value::Float(n) => format!("(f64.const {n})"),
+            Value::Bool(n) => Value::Integer(if *n { 1 } else { 0 }).compile(ctx)?,
+            Value::String(str) => {
+                let result = Expr::Literal(Value::Integer(ctx.index.clone())).compile(ctx)?;
+                ctx.data.push(format!(r#"(data {} "{str}")"#, result));
+                ctx.index += str.len() as i32;
+                result
+            }
+        })
+    }
+
+    fn type_infer(&self, _: &mut Compiler) -> Option<Type> {
+        Some(match self {
+            Value::Integer(_) => Type::Integer,
+            Value::Float(_) => Type::Float,
+            Value::Bool(_) => Type::Bool,
+            Value::String(_) => Type::Pointer,
+        })
+    }
+}
+
 #[derive(Clone, Debug)]
 pub enum Type {
     Integer,
