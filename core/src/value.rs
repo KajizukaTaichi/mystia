fc350138a36f71ed0a37775ce9bb84d0174b7396
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-25 18:01:57 +0900

    Update value.rs

--- core/src/value.rs
+++ core/src/value.rs
@@ -2,7 +2,7 @@ use crate::*;
 
 #[derive(Debug, Clone)]
 pub enum Value {
-    Nimber(f64),
+    Number(f64),
     Bool(bool),
     Array(i32, usize),
     String(String),
@@ -29,9 +29,9 @@ impl Node for Value {
 
     fn compile(&self, ctx: &mut Compiler) -> Option<String> {
         Some(match self {
-            Value::Integer(n) | Value::Array(n, _) => format!("(i32.const {n})"),
-            Value::Float(n) => format!("(f64.const {n})"),
-            Value::Bool(n) => Value::Integer(if *n { 1 } else { 0 }).compile(ctx)?,
+            Value::Array(n, _) => format!("(i32.const {n})"),
+            Value::Number(n) => format!("(f64.const {n})"),
+            Value::Bool(n) => Value::Array(if *n { 1 } else { 0 }, 0).compile(ctx)?,
             Value::String(str) => {
                 let result = Value::Array(ctx.alloc_index.clone(), str.len()).compile(ctx)?;
                 ctx.static_data
@@ -44,9 +44,9 @@ impl Node for Value {
 
     fn type_infer(&self, _: &mut Compiler) -> Option<Type> {
         Some(match self {
-            Value::Integer(_) => Type::Integer,
-            Value::String(_) => Type::Pointer,
-            Value::Float(_) => Type::Float,
+            Value::Number(_) => Type::Number,
+            Value::String(_) => Type::String,
+            Value::Array(_, _) => Type::Array,
             Value::Bool(_) => Type::Bool,
         })
     }
