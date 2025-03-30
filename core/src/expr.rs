5017ca21719ccb688a1d146d8f059ec8590fcc42
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-30 12:18:54 +0900

    Update expr.rs

--- core/src/expr.rs
+++ core/src/expr.rs
@@ -96,23 +96,26 @@ impl Node for Expr {
             }
             Expr::Dict(dict) => {
                 let mut result: Vec<_> = vec![];
+                let mut table = IndexMap::new();
                 let Type::Dict(infered) = self.type_infer(ctx)? else {
                     return None;
                 };
                 ctx.pointer_index = ctx.alloc_index;
-                for (_, elm) in dict {
+                for (name, elm) in dict {
                     let typ = elm.type_infer(ctx)?;
+                    table.insert(name.to_string(), ctx.alloc_index);
                     result.push(format!(
                         "({type}.store {address} {value})",
                         r#type = typ.clone().compile(ctx)?,
-                        address = Value::Dict(ctx.alloc_index, infered.clone()).compile(ctx)?,
+                        address = Value::Dict(ctx.alloc_index, table.clone(), infered.clone())
+                            .compile(ctx)?,
                         value = elm.compile(ctx)?
                     ));
                     ctx.alloc_index += typ.bytes_length();
                 }
                 format!(
                     "{} {}",
-                    Value::Dict(ctx.pointer_index, infered).compile(ctx)?,
+                    Value::Dict(ctx.alloc_index, table, infered).compile(ctx)?,
                     join!(result)
                 )
             }
