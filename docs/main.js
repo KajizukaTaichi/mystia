750f2ddbd54a8520af6b4fd8945fd97e05abc067
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-05 02:21:06 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,36 +1,22 @@
 import init, { mystia as compile } from "./pkg/mystia_wasm.js";
 
 await init();
-
-class MystiaValue {
-    constructor(instance) {
-        this.instance = instance;
-    }
-
-    getValue() {
-        return this.instance.exports._start();
-    }
-
-    invoke(name) {
-        return this.instance.exports[name]();
-    }
-
-    getString() {
-        const address = this.getValue();
+export async function mystia(code) {
+    const compileResult = compile(code);
+    const type = compileResult.get_return_type();
+    const bytecodes = compileResult.get_bytecode().buffer;
+    const { instance } = await WebAssembly.instantiate(bytecodes);
+    const returns = instance.exports._start();
+    if (type == "str") {
+        let stringLength = returns;
         const memoryView = new Uint8Array(this.instance.exports.mem.buffer);
-
-        let stringLength = address;
         while (memoryView[stringLength] !== 0) {
             stringLength++;
         }
         const stringBytes = memoryView.slice(address, stringLength);
         const textDecoder = new TextDecoder("utf-8");
         return textDecoder.decode(stringBytes);
+    } else if (type == "int" || type == "num") {
+        return returns.toString();
     }
-}
-
-export async function mystia(code) {
-    const bytecodes = compile(code).buffer;
-    const { instance } = await WebAssembly.instantiate(bytecodes);
-    return new MystiaValue(instance);
 }
