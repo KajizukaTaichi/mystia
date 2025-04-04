a407607d7f2167fe978254d75aadbd30e35f951f
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-05 02:14:31 +0900

    Update lib.rs

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -2,10 +2,27 @@ use mystia_core::Compiler;
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
-pub fn mystia(source: &str) -> Option<Vec<u8>> {
-    if let Some(wat_code) = Compiler::new().build(source) {
+pub struct Result(Vec<u8>, String);
+
+#[wasm_bindgen]
+impl Result {
+    #[wasm_bindgen]
+    pub fn get_bytecode(&self) -> Vec<u8> {
+        self.0.clone()
+    }
+
+    #[wasm_bindgen]
+    pub fn get_return_type(&self) -> String {
+        self.1.clone()
+    }
+}
+
+#[wasm_bindgen]
+pub fn mystia(source: &str) -> Option<Result> {
+    let mut compiler = Compiler::new();
+    if let Some(wat_code) = compiler.build(source) {
         if let Ok(bytes) = wat::parse_str(wat_code) {
-            Some(bytes)
+            Some(Result(bytes, compiler.program_return.format()))
         } else {
             None
         }
