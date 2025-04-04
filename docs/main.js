7ea1c395e7486124e59f0df9618351d59096392e
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-05 02:25:08 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -9,14 +9,16 @@ export async function mystia(code) {
     const returns = instance.exports._start();
     if (type == "str") {
         let stringLength = returns;
-        const memoryView = new Uint8Array(this.instance.exports.mem.buffer);
+        const memoryView = new Uint8Array(instance.exports.mem.buffer);
         while (memoryView[stringLength] !== 0) {
             stringLength++;
         }
-        const stringBytes = memoryView.slice(address, stringLength);
+        const stringBytes = memoryView.slice(returns, stringLength);
         const textDecoder = new TextDecoder("utf-8");
-        return textDecoder.decode(stringBytes);
+        return `"${textDecoder.decode(stringBytes)}"`;
     } else if (type == "int" || type == "num") {
         return returns.toString();
+    } else if (type == "bool") {
+        return returns == 1 ? "true" : "false";
     }
 }
