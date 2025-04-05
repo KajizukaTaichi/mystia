a5a85908105c6d7c52bfcd68acfb4e49ca6faec2
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-05 20:47:15 +0900

    Update utils.rs

--- core/src/utils.rs
+++ core/src/utils.rs
@@ -39,12 +39,12 @@ macro_rules! config_return {
 #[macro_export]
 macro_rules! if_ptr {
     ($typ: expr, $block: block) => {
-        if let Type::String(_) | Type::Array(_, _) | Type::Dict(_) = $typ {
+        if let Type::String | Type::Array(_, _) | Type::Dict(_) = $typ {
             $block;
         }
     };
     ($typ: expr, $block: block, $els: block) => {
-        if let Type::String(_) | Type::Array(_, _) | Type::Dict(_) = $typ {
+        if let Type::String | Type::Array(_, _) | Type::Dict(_) = $typ {
             $block;
         } else {
             $els;
