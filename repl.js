262cfdb7fbb4b9b7126edee2580224ce6ce558f6
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-06 17:37:36 +0900

    Update repl.js

--- repl.js
+++ repl.js
@@ -1,23 +1,37 @@
 import { createInterface } from "readline";
 import { mystia } from "./docs/node/main.js";
 
-console.clear();
 const rl = createInterface({
     input: process.stdin,
     output: process.stdout,
+    prompt: "> ",
 });
 
 console.log("Mystia REPL");
-function mainloop() {
-    rl.question("You: ", async (code) => {
-        try {
-            console.log(`Mystia: ${await mystia(code)}`);
-        } catch {
-            console.error(
-                "Error occurred during compilation. Check out if the program is correct",
-            );
-        }
+
+// プロンプト表示
+rl.prompt();
+
+// ユーザーからの入力を受け取り、評価して表示する
+rl.on("line", async (input) => {
+    // 'exit' と入力された場合に終了
+    if (input.trim() === "exit") {
         rl.close();
-        mainloop();
-    });
-}
+        return;
+    }
+
+    try {
+        mystia(code).then((x) => console.log(x));
+    } catch {
+        console.error(
+            "Error occurred during compilation. Check out if the program is correct",
+        );
+    }
+    rl.prompt();
+});
+
+// REPLを閉じる時の処理
+rl.on("close", () => {
+    console.log("REPLを終了します。");
+    process.exit(0);
+});
