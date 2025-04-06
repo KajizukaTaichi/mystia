ff626a51caa79bb9747d610603049a4a662fa719
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-06 20:42:19 +0900

    Update main.js

--- docs/web/main.js
+++ docs/web/main.js
@@ -11,18 +11,32 @@ export async function mystia(code) {
 }
 
 function ffi(type, value) {
-    if (type == "str") {
-        let stringLength = value;
+    if (type == "int" || type == "num") {
+        return value;
+    } else if (type == "bool") {
+        return value != 0;
+    } else if (type == "str") {
         const memoryView = new Uint8Array(instance.exports.mem.buffer);
+        let stringLength = value;
         while (memoryView[stringLength] !== 0) {
             stringLength++;
         }
         const stringBytes = memoryView.slice(value, stringLength);
         const textDecoder = new TextDecoder("utf-8");
         return textDecoder.decode(stringBytes);
-    } else if (type == "int" || type == "num") {
-        return value;
-    } else if (type == "bool") {
-        return value != 0;
+    } else if (type.startsWith("[") && type.endsWith("]")) {
+        let result = [];
+        const [innerType, length] = rsplitOnce(";");
+        const bytes = innerType == "num" ? Uint16Array : Uint8Array;
+        const memoryView = new bytes(instance.exports.mem.buffer);
+        for (let index = 0; index < length; index++) {
+            result.push(ffi(innerType), memoryView[index]);
+        }
+        return result;
     }
+}
+
+function rsplitOnce(str, delimiter) {
+    const idx = str.lastIndexOf(delimiter);
+    return [str.slice(0, idx), str.slice(idx + delimiter.length)];
 }
