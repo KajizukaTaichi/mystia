4e5c281e19fd6445dfa2abe8c6cd65e2520675dc
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-06 16:17:37 +0900

    Create mystia_wasm.d.ts

---
+++ docs/node/mystia_wasm.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export function mystia(source: string): Result | undefined;
+export class Result {
+  private constructor();
+  free(): void;
+  get_bytecode(): Uint8Array;
+  get_return_type(): string;
+}
