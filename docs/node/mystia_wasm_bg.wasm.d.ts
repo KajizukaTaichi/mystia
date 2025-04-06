a1e6b498b69bf5d2971370be46de6fd91790cc8d
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-06 16:17:39 +0900

    Create mystia_wasm_bg.wasm.d.ts

---
+++ docs/node/mystia_wasm_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_result_free: (a: number, b: number) => void;
+export const result_get_bytecode: (a: number) => [number, number];
+export const result_get_return_type: (a: number) => [number, number];
+export const mystia: (a: number, b: number) => number;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_start: () => void;
