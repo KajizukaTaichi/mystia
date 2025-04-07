c9d172d5dcd4453b4c7f35cb2a877a39f82c2505
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-07 16:43:49 +0900

    Update node.js

--- docs/binding/node.js
+++ docs/binding/node.js
@@ -1,4 +1,5 @@
 import { mystia as compile } from "../node/mystia_wasm.js";
+import { ffi } from "./ffi.js";
 
 export async function mystia(code) {
     const result = compile(code);
@@ -7,40 +8,4 @@ export async function mystia(code) {
     const { instance } = await WebAssembly.instantiate(bytecodes);
     const value = instance.exports._start();
     return ffi(instance, type, value);
-}
-
-function ffi(instance, type, value) {
-    if (type == "int" || type == "num") {
-        return value;
-    } else if (type == "bool") {
-        return value != 0;
-    } else if (type == "str") {
-        const memoryView = new Uint8Array(instance.exports.mem.buffer);
-        let stringLength = value;
-        while (memoryView[stringLength] !== 0) {
-            stringLength++;
-        }
-        const stringBytes = memoryView.slice(value, stringLength);
-        const textDecoder = new TextDecoder("utf-8");
-        return textDecoder.decode(stringBytes);
-    } else if (type.startsWith("[") && type.endsWith("]")) {
-        const token = type.slice(1, type.length);
-        const [innerType, lengthSource] = rsplitOnce(token, ";");
-        const length = parseInt(lengthSource.trim());
-        const [arrayClass, byte] =
-            innerType == "num" ? [BigInt64Array, 8] : [Int32Array, 4];
-        const memoryView = new arrayClass(instance.exports.mem.buffer);
-        const pointer = value / byte;
-        let [result, index] = [[], pointer];
-        while (index < pointer + length) {
-            result.push(ffi(instance, innerType, memoryView[index]));
-            index++;
-        }
-        return result;
-    }
-}
-
-function rsplitOnce(str, delimiter) {
-    const idx = str.lastIndexOf(delimiter);
-    return [str.slice(0, idx), str.slice(idx + delimiter.length)];
 }
