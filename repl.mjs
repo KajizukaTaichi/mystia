5c2937afc8296672ae5f69bfded1d5e4c18818c2
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-18 04:54:49 +0900

    Let JS code be module

---
+++ repl.mjs
@@ -0,0 +1,35 @@
+import { createInterface } from "readline";
+import { mystia } from "./docs/binding/node.mjs";
+
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "> ",
+});
+
+let code = "";
+
+console.log("Mystia REPL");
+rl.prompt();
+
+rl.on("line", (input) => {
+    if (input.trim() !== "") {
+        mystia(`${code};${input}`)
+            .then((result) => {
+                if (result === undefined) {
+                    code += `;${input}`;
+                } else {
+                    console.log(result);
+                }
+            })
+            .catch((e) => console.log(e))
+            .then(() => rl.prompt());
+    } else {
+        rl.prompt();
+    }
+});
+
+rl.on("close", () => {
+    console.log("Bye");
+    process.exit(0);
+});
