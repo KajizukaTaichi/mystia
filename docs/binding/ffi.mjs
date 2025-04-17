aabfc3378dd77ad59a494bd8b428a2604b92ad8b
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-18 06:34:50 +0900

    Update ffi.mjs

--- docs/binding/ffi.mjs
+++ docs/binding/ffi.mjs
@@ -27,9 +27,18 @@ export function ffi(instance, type, value) {
     } else if (type.type == "dict") {
         let result = {};
         const memoryView32bit = new Int32Array(instance.exports.mem.buffer);
+        const memoryView64bit = new Float64Array(instance.exports.mem.buffer);
         for (let [name, field] of Object.entries(type.fields)) {
-            const address = value + field.offset / 4;
-            result[name] = ffi(instance, field.type, memoryView32bit[address]);
+            const address = value + field.offset;
+            const [memoryView, byte] =
+                field.type == "num"
+                    ? [memoryView64bit, 8]
+                    : [memoryView32bit, 4];
+            result[name] = ffi(
+                instance,
+                field.type,
+                memoryView[address / byte],
+            );
         }
         return result;
     }
