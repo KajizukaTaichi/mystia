cf2bc1ba9c3673a784d1547f81e8e400f15b9bd5
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-18 05:16:51 +0900

    Update ffi.mjs

--- docs/binding/ffi.mjs
+++ docs/binding/ffi.mjs
@@ -12,10 +12,8 @@ export function ffi(instance, type, value) {
         const stringBytes = memoryView.slice(value, stringLength);
         const textDecoder = new TextDecoder("utf-8");
         return textDecoder.decode(stringBytes);
-    } else if (type == "array") {
-        const token = type.slice(1, type.length);
-        const [innerType, lengthSource] = rsplitOnce(token, ";");
-        const length = parseInt(lengthSource.trim());
+    } else if (type.type == "array") {
+        const [innerType, length] = [type.element, type.length];
         const [arrayClass, byte] =
             innerType == "num" ? [Float64Array, 8] : [Int32Array, 4];
         const memoryView = new arrayClass(instance.exports.mem.buffer);
@@ -24,6 +22,20 @@ export function ffi(instance, type, value) {
         while (index < pointer + length) {
             result.push(ffi(instance, innerType, memoryView[index]));
             index++;
+        }
+        return result;
+    } else if (type.type == "dict") {
+        let result = {};
+        const memoryView32bit = new Int32Array(instance.exports.mem.buffer);
+        const memoryView64bit = new Float64Array(instance.exports.mem.buffer);
+        for (let [name, field] of type.fields.entries()) {
+            result[name] = ffi(
+                instance,
+                field.type,
+                field.type == "num"
+                    ? memoryView64bit[value + field.offset]
+                    : memoryView32bit[value + field.offset],
+            );
         }
         return result;
     }
