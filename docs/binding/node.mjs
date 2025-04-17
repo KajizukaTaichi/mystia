3848e3b6e4e080d034ad7634494d74b1f8d8b025
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-18 04:54:29 +0900

    Create node.mjs

---
+++ docs/binding/node.mjs
@@ -0,0 +1,11 @@
+import { mystia as compile } from "../node/mystia_wasm.js";
+import { ffi } from "./ffi.mjs";
+
+export async function mystia(code) {
+    const result = compile(code);
+    const type = result.get_return_type();
+    const bytecodes = result.get_bytecode().buffer;
+    const { instance } = await WebAssembly.instantiate(bytecodes);
+    const value = instance.exports._start();
+    return ffi(instance, type, value);
+}
