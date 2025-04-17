40ba3180af234275f8e53a2867f430b8a4282ded
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-18 06:19:02 +0900

    Update node.mjs

--- docs/binding/node.mjs
+++ docs/binding/node.mjs
@@ -4,6 +4,9 @@ import { ffi } from "./ffi.mjs";
 export async function mystia(code) {
     const result = compile(code);
     const type = eval(`(${result.get_return_type()})`);
+    if (type == null) {
+        return null;
+    }
     const bytecodes = result.get_bytecode().buffer;
     const { instance } = await WebAssembly.instantiate(bytecodes);
     const value = instance.exports._start();
