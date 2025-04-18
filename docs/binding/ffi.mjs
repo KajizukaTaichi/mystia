c757437a7eabf1bce2eb613b2bcb54ea5c370d26
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-18 16:32:40 +0900

    Update ffi.mjs

--- docs/binding/ffi.mjs
+++ docs/binding/ffi.mjs
@@ -26,20 +26,28 @@ export function ffi(instance, type, value) {
         return result;
     } else if (type.type == "dict") {
         let result = {};
-        const memoryView32bit = new Int32Array(instance.exports.mem.buffer);
-        const memoryView64bit = new Float64Array(instance.exports.mem.buffer);
+        const memoryView = new Int32Array(instance.exports.mem.buffer);
         for (let [name, field] of Object.entries(type.fields)) {
-            const address = value + field.offset;
-            const [memoryView, byte] =
-                field.type == "num"
-                    ? [memoryView64bit, 8]
-                    : [memoryView32bit, 4];
+            const address = (value + field.offset) / 4;
             result[name] = ffi(
                 instance,
                 field.type,
-                memoryView[address / byte],
+                field.type == "num"
+                    ? int32PairToFloat64(
+                          memoryView[address],
+                          memoryView[address + 1],
+                      )
+                    : memoryView[Math.floor(address)],
             );
         }
         return result;
     }
+}
+
+function int32PairToFloat64(a, b) {
+    const buffer = new ArrayBuffer(8);
+    const view = new DataView(buffer);
+    view.setInt32(0, a, true);
+    view.setInt32(4, b, true);
+    return view.getFloat64(0, true);
 }
