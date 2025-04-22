960e33e988bba48ba5670adf829c33f613fe4981
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-22 13:07:01 +0900

    Update oper.rs

--- core/src/oper.rs
+++ core/src/oper.rs
@@ -91,7 +91,7 @@ impl Node for Oper {
             }
             Oper::Cast(lhs, rhs) => {
                 lhs.type_infer(ctx)?;
-                Some(rhs.clone())
+                Some(rhs.solve_alias(ctx)?.clone())
             }
         }
     }
