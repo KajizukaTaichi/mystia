c66d648e524fe7505a217326f4401177b630c4a0
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-24 09:28:06 +0900

    Update ffi.mjs

--- docs/binding/ffi.mjs
+++ docs/binding/ffi.mjs
@@ -42,8 +42,8 @@ export function ffi(instance, type, value) {
             result[name] = ffi(instance, field.type, value);
         }
         return result;
-    } else if (type.type == "null") {
-        return 0;
+    } else {
+        return type;
     }
 }
 
