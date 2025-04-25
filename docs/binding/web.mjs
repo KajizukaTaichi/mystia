daa521b84f42b80df90fe2dbd1749d0b7a0c54d8
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-25 15:24:26 +0900

    Update web.mjs

--- docs/binding/web.mjs
+++ docs/binding/web.mjs
@@ -5,8 +5,8 @@ await init();
 export async function mystia(code) {
     const result = compile(code);
     const type = eval(`(${result.get_return_type()})`);
-    if (type == null) return null;
     const bytecodes = result.get_bytecode().buffer;
+    if (type == null) return null;
 
     let mystiaAlert, mystiaConfirm, mystiaPrompt, mystiaDraw;
     const { instance } = await WebAssembly.instantiate(bytecodes, {
@@ -33,6 +33,7 @@ export async function mystia(code) {
         ctx.fillRect(x * 100, y * 100, 100, 100);
     };
 
+    console.log(instance.exports.draw);
     const value = instance.exports._start();
     return ffi(instance, type, value);
 }
