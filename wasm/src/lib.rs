abd23327e2c2fac765e103d06202fdb2aa772789
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-26 20:15:39 +0900

    Update lib.rs

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -2,18 +2,27 @@ use mystia_core::Compiler;
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
-pub struct Result(Vec<u8>, String);
+pub struct Result {
+    bytecode: Vec<u8>,
+    wat_code: String,
+    return_type: String,
+}
 
 #[wasm_bindgen]
 impl Result {
     #[wasm_bindgen]
     pub fn get_bytecode(&self) -> Vec<u8> {
-        self.0.clone()
+        self.bytecode.clone()
+    }
+
+    #[wasm_bindgen]
+    pub fn get_wat_code(&self) -> String {
+        self.wat_code.clone()
     }
 
     #[wasm_bindgen]
     pub fn get_return_type(&self) -> String {
-        self.1.clone()
+        self.return_type.clone()
     }
 }
 
@@ -21,8 +30,12 @@ impl Result {
 pub fn mystia(source: &str) -> Option<Result> {
     let mut compiler = Compiler::new();
     if let Some(wat_code) = compiler.build(source) {
-        if let Ok(bytes) = wat::parse_str(wat_code) {
-            Some(Result(bytes, compiler.program_return.ffi_json()))
+        if let Ok(bytes) = wat::parse_str(wat_code.clone()) {
+            Some(Result {
+                bytecode: bytes,
+                wat_code: wat_code.to_string(),
+                return_type: compiler.program_return.ffi_json(),
+            })
         } else {
             None
         }
