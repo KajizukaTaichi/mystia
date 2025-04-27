8dfd0ffeaa10a91354e249269ec5d200099c2b00
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-27 09:45:06 +0900

    Update lib.rs

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,4 +1,4 @@
-use mystia_core::Compiler;
+use mystia_core::{Compiler, Type};
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
@@ -27,12 +27,44 @@ pub fn mystia(source: &str) -> Option<Result> {
         if let Ok(bytes) = wat::parse_str(wat_code.clone()) {
             Some(Result {
                 bytecode: bytes,
-                return_type: compiler.program_return.ffi_json(),
+                return_type: type_to_json(&compiler.program_return),
             })
         } else {
             None
         }
     } else {
         None
+    }
+}
+
+pub fn type_to_json(typ: &Type) -> String {
+    match typ {
+        Type::Integer => "\"int\"".to_string(),
+        Type::Number => "\"num\"".to_string(),
+        Type::Bool => "\"bool\"".to_string(),
+        Type::String => "\"str\"".to_string(),
+        Type::Void => "null".to_string(),
+        Type::Dict(dict) => format!(
+            "{{ type: \"dict\", fields: {{ {} }} }}",
+            dict.iter()
+                .map(|(k, (offset, typ))| format!(
+                    "{k}: {{ type: {}, offset: {offset} }}",
+                    typ.ffi_json()
+                ))
+                .collect::<Vec<_>>()
+                .join(", ")
+        ),
+        Type::Array(typ, len) => format!(
+            "{{ type: \"array\", element: {}, length: {len} }}",
+            typ.ffi_json()
+        ),
+        Type::Enum(e) => format!(
+            "{{ type: \"enum\", enum: [{}] }}",
+            e.iter()
+                .map(|x| format!("\"{x}\""))
+                .collect::<Vec<_>>()
+                .join(", ")
+        ),
+        Type::Alias(name) => format!("{{ type: \"alias\", name: {name} }}"),
     }
 }
