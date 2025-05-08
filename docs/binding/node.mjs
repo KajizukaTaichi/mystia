3b3639c1e71d127bb376f6f1d2d8a0414631fce2
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-08 14:04:12 +0900

    Update node.mjs

--- docs/binding/node.mjs
+++ docs/binding/node.mjs
@@ -8,14 +8,32 @@ export async function mystia(code) {
 
     let mystiaFunctions = {
         print: null,
+        int_to_str: null,
+        concat: null,
+        rand: null,
     };
     const { instance } = await WebAssembly.instantiate(bytecodes, {
         env: {
             print: (ptr) => mystiaFunctions.print(ptr),
+            int_to_str: (num) => mystiaFunctions.int_to_str(num),
+            concat: (str1, str2) => mystiaFunctions.concat(str1, str2),
+            rand: () => mystiaFunctions.rand(),
         },
     });
-    mystiaFunctions.print = (ptr) => console.log(read(instance, "str", ptr));
-
+    mystiaFunctions.print = (ptr) => {
+        return console.log(read(instance, "str", ptr));
+    };
+    mystiaFunctions.int_to_str = (value) => {
+        return mystia_write(instance, "str", value.toString());
+    };
+    mystiaFunctions.concat = (str1, str2) => {
+        const strs1 = mystia_read(instance, "str", str1);
+        const strs2 = mystia_read(instance, "str", str2);
+        return mystia_write(instance, "str", strs1 + strs2);
+    };
+    mystiaFunctions.rand = () => {
+        return Math.random();
+    };
     const value = instance.exports._start();
     return read(instance, type, value);
 }
