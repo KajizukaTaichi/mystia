bb0ce546b99f2f6bd046430a1d818dbc2ffccc42
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-19 13:45:09 +0900

    Create main.js

---
+++ docs/main.js
@@ -0,0 +1,51 @@
+import { mystia } from "./binding/web.mjs";
+let timer;
+
+const codeEditor = document.getElementById("code");
+const resultArea = document.getElementById("result");
+const runBtn = document.getElementById("run");
+const buildBtn = document.getElementById("build");
+
+const timerLabel = document.getElementById("timer");
+const exampleFieldInput = document.getElementById("example-field");
+const exampleSelectBtn = document.getElementById("example-select");
+
+exampleSelectBtn.addEventListener("click", async () => {
+    const exampleCodeUrl = `https://raw.githubusercontent.com/KajizukaTaichi/mystia/refs/heads/master/example/${exampleFieldInput.value}.ms`;
+    const response = await fetch(exampleCodeUrl);
+    codeEditor.value = await response.text();
+});
+
+runBtn.addEventListener("click", async (e) => {
+    const startTime = Date.now();
+    timer = setInterval(() => {
+        timerLabel.textContent = `Time: ${Date.now() - startTime}ms`;
+    }, 1);
+    try {
+        const result = await mystia(codeEditor.value);
+        resultArea.innerHTML = JSON.stringify(result, null, 2);
+    } catch (error) {
+        resultArea.innerHTML = error;
+    }
+    clearInterval(timer);
+});
+
+buildBtn.addEventListener("click", () => {
+    const data = `
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Mystia App</title>
+            </head>
+            <body>
+                <script type="module" src="https://kajizukataichi.github.io/mystia/izakaya.mjs"></\script>
+                <mystia-code>${codeEditor.value}</mystia-code>
+            </body>
+        </html>
+    `;
+    const blob = new Blob([data], { type: "text/plain" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "mystia-app.html";
+    link.click();
+});
