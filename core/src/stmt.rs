f3326451c02c6e3ebada0328dcb29aaeac353977
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-22 16:18:28 +0900

    Update stmt.rs

--- core/src/stmt.rs
+++ core/src/stmt.rs
@@ -128,33 +128,33 @@ impl Node for Stmt {
                         format!("(global.set ${name} {})", value.compile(ctx)?)
                     }
                 },
+                Expr::Call(name, _) => {
+                    let function = ctx.function_type.get(&name)?.clone();
+                    let [var_typ, arg_typ] = [ctx.variable_type.clone(), ctx.argument_type.clone()];
+                    ctx.variable_type = function.variables.clone();
+                    ctx.argument_type = function.arguments.clone();
+                    let code = format!(
+                        "(func ${name} {pub} {args} {ret} {locals} {body})",
+                        args = join!(
+                            &function
+                                .arguments
+                                .iter()
+                                .map(|(name, typ)| Some(format!(
+                                    "(param ${name} {})",
+                                    typ.type_infer(ctx)?.compile(ctx)?
+                                )))
+                                .collect::<Option<Vec<_>>>()?
+                        ),
+                        ret = compile_return!(function.returns, ctx),
+                        pub = if let Scope::Global = scope { format!("(export \"{name}\")") } else { String::new() },
+                        body = value.compile(ctx)?, locals = expand_local(ctx)?
+                    );
+                    [ctx.variable_type, ctx.argument_type] = [var_typ, arg_typ];
+                    ctx.declare_code.push(code);
+                    String::new()
+                }
                 Expr::Oper(oper) => match *oper.clone() {
-                    Oper::Cast(Expr::Call(name, _), _) => {
-                        let function = ctx.function_type.get(&name)?.clone();
-                        let [var_typ, arg_typ] =
-                            [ctx.variable_type.clone(), ctx.argument_type.clone()];
-                        ctx.variable_type = function.variables.clone();
-                        ctx.argument_type = function.arguments.clone();
-                        let code = format!(
-                            "(func ${name} {pub} {args} {ret} {locals} {body})",
-                            args = join!(
-                                &function
-                                    .arguments
-                                    .iter()
-                                    .map(|(name, typ)| Some(format!(
-                                        "(param ${name} {})",
-                                        typ.type_infer(ctx)?.compile(ctx)?
-                                    )))
-                                    .collect::<Option<Vec<_>>>()?
-                            ),
-                            ret = compile_return!(function.returns, ctx),
-                            pub = if let Scope::Global = scope { format!("(export \"{name}\")") } else { String::new() },
-                            body = value.compile(ctx)?, locals = expand_local(ctx)?
-                        );
-                        [ctx.variable_type, ctx.argument_type] = [var_typ, arg_typ];
-                        ctx.declare_code.push(code);
-                        String::new()
-                    }
+                    Oper::Cast(func, _) => Stmt::Let(*scope, func, value.clone()).compile(ctx)?,
                     _ => return None,
                 },
                 Expr::Index(array, index) => {
@@ -241,6 +241,22 @@ impl Node for Stmt {
                             }
                         }
                     },
+                    Expr::Call(name, args) => {
+                        let var_typ = ctx.variable_type.clone();
+                        let arg_typ = ctx.argument_type.clone();
+                        let ret_typ = value.type_infer(ctx)?;
+                        compile_args!(args, ctx);
+                        ctx.function_type.insert(
+                            name.to_owned(),
+                            Function {
+                                variables: ctx.variable_type.clone(),
+                                arguments: ctx.argument_type.clone(),
+                                returns: ret_typ,
+                            },
+                        );
+                        ctx.variable_type = var_typ;
+                        ctx.argument_type = arg_typ;
+                    }
                     Expr::Oper(oper) => match *oper.clone() {
                         Oper::Cast(Expr::Call(name, args), ret) => {
                             let var_typ = ctx.variable_type.clone();
