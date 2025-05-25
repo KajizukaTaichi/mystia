d7fcb1448ee91f2975ee9d8f5d301c69b479d9c9
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-25 21:10:01 +0900

    Update node.mjs

--- docs/runtime/node.mjs
+++ docs/runtime/node.mjs
@@ -2,6 +2,7 @@ import { mystia as compile } from "../wasm/node/mystia_wasm.js";
 import { MystiaNodeLib } from "./lib.mjs";
 import { MathLib } from "./math.mjs";
 import { OSLib } from "./os.mjs";
+import { module } from "./module.mjs";
 import { read } from "./ffi.mjs";
 
 const MODULE_CLASSES = {
@@ -10,48 +11,19 @@ const MODULE_CLASSES = {
 };
 
 export async function mystia(code, customModules = {}) {
-  const result = compile(code);
-  const returnType = eval(`(${result.get_return_type()})`);
-  const bytecodes = result.get_bytecode().buffer;
-  const moduleObj = await WebAssembly.compile(bytecodes);
-  const importsInfo = WebAssembly.Module.imports(moduleObj);
+    const result = compile(code);
+    const returnType = eval(`(${result.get_return_type()})`);
+    const bytecodes = result.get_bytecode().buffer;
+    const moduleObj = await WebAssembly.compile(bytecodes);
+    const importsInfo = WebAssembly.Module.imports(moduleObj);
+    const stdLib = new MystiaNodeLib();
+    const importObject = { env: { ...stdLib.bridge() } };
+    const instances = { MystiaNodeLib: stdLib };
+    module(importsInfo, MODULE_CLASSES, customModules, instances, importObject);
 
-  const stdLib = new MystiaNodeLib();
-  const importObject = { env: { ...stdLib.bridge() } };
-  const instances = { MystiaNodeLib: stdLib };
-
-  const moduleNames = new Set(
-    importsInfo
-      .filter(i => i.module === "env" && i.kind === "func")
-      .map(i => i.name.split(".")[0])
-  );
-
-  for (const { module, name, kind } of importsInfo) {
-    if (module !== "env") continue;
-    let modName, fnName, key;
-    if (name.includes(".")) {
-        [modName, fnName] = name.split(".");
-        key = name;
-    } else {
-        modName = "MystiaNodeLib";
-        fnName = name;
-        key = fnName;
-    }
-    const instanceObj =
-      customModules[modName] ?? instances[modName] ?? (MODULE_CLASSES[modName] && new MODULE_CLASSES[modName]());
-    if (!instanceObj) {
-      throw new Error(`Unknown import module: ${modName}`);
-    }
-    const bridge = instanceObj.bridge();
-    if (!(fnName in bridge)) {
-        throw new Error(`Function ${fnName} not found in module ${modName}`);
-    }
-    importObject.env[key] = bridge[fnName];
-    instances[modName] = instanceObj;
-  }
-  const wab = bytecodes;
-  const { instance } = await WebAssembly.instantiate(wab, importObject);
-  Object.values(instances).forEach(inst => inst.set_wasm(instance));
-  const raw = instance.exports._start();
-  return read(instance, returnType, raw);
+    const wab = bytecodes;
+    const { instance } = await WebAssembly.instantiate(wab, importObject);
+    Object.values(instances).forEach((inst) => inst.set_wasm(instance));
+    const raw = instance.exports._start();
+    return read(instance, returnType, raw);
 }
