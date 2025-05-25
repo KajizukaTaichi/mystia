956cfd4933d122d348a0648b096b61535742c18f
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-25 21:09:59 +0900

    Create module.mjs

---
+++ docs/runtime/module.mjs
@@ -0,0 +1,35 @@
+export function module(
+    importsInfo,
+    customModules = {},
+    MODULE_CLASSES,
+    instances,
+    importObject,
+) {
+    for (const { module, name, kind: _ } of importsInfo) {
+        if (module !== "env") continue;
+        let modName, fnName, key;
+        if (name.includes(".")) {
+            [modName, fnName] = name.split(".");
+            key = name;
+        } else {
+            modName = "MystiaNodeLib";
+            fnName = name;
+            key = fnName;
+        }
+        const instanceObj =
+            new customModules[modName]() ??
+            new instances[modName]() ??
+            new MODULE_CLASSES[modName]();
+        if (!instanceObj) {
+            throw new Error(`Unknown import module: ${modName}`);
+        }
+        const bridge = instanceObj.bridge();
+        if (!(fnName in bridge)) {
+            throw new Error(
+                `Function ${fnName} not found in module ${modName}`,
+            );
+        }
+        importObject.env[key] = bridge[fnName];
+        instances[modName] = instanceObj;
+    }
+}
