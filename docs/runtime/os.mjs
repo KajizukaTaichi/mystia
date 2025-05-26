b495e6298d2a7411a10407df9ea2274cab7686b3
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-26 17:23:10 +0900

    Update os.mjs

--- docs/runtime/os.mjs
+++ docs/runtime/os.mjs
@@ -5,69 +5,55 @@ import path from "path";
 export class OSLib {
     constructor() {
         this.functions = {
-            getcwd: () => {
-                return write(this.instance, "str", process.cwd());
-            },
-            remove: (value) => {
-                fs.unlinkSync(read(this.instance, "str", value));
-            },
-            mkdir: (value) => {
-                fs.mkdirSync(read(this.instance, "str", value));
-            },
-            rename: (src, dest) => {
-                return fs.renameSync(
+            getcwd: () => write(this.instance, "str", process.cwd()),
+            remove: (value) => fs.unlinkSync(read(this.instance, "str", value)),
+            mkdir: (value) => fs.mkdirSync(read(this.instance, "str", value)),
+            rename: (src, dest) =>
+                fs.renameSync(
                     read(this.instance, "str", src),
                     read(this.instance, "str", dest),
-                );
-            },
-            chdir: (value) => {
-                return write(
+                ),
+            chdir: (value) =>
+                write(
                     this.instance,
                     "str",
                     process.chdir(read(this.instance, "str", value)),
-                );
-            },
-            listdir: (value) => {
-                return write(
+                ),
+            listdir: (value) =>
+                write(
                     this.instance,
                     "str",
                     fs.readdirSync(read(this.instance, "str", value)),
-                );
-            },
-            path_join: (value, end) => {
-                return write(
+                ),
+            path_join: (value, end) =>
+                write(
                     this.instance,
                     "str",
                     path.join(
                         read(this.instance, "str", value),
                         read(this.instance, "str", end),
                     ),
-                );
-            },
-            path_basename: (value) => {
-                return write(
+                ),
+            path_basename: (value) =>
+                write(
                     this.instance,
                     "str",
                     path.basename(read(this.instance, "str", value)),
-                );
-            },
-            path_parent: (value) => {
-                return write(
+                ),
+            path_parent: (value) =>
+                write(
                     this.instance,
                     "str",
                     path.dirname(read(this.instance, "str", value)),
-                );
-            },
-            path_abs: (value) => {
-                return write(
+                ),
+            path_abs: (value) =>
+                write(
                     this.instance,
                     "str",
                     path.resolve(read(this.instance, "str", value)),
-                );
-            },
-            path_exist: (value) => {
-                return fs.existsSync(read(this.instance, "str", value));
-            },
+                ),
+            path_exist: (value) =>
+                fs.existsSync(read(this.instance, "str", value)),
             path_isfile: (value) => {
                 let isFile = false;
                 try {
@@ -90,16 +76,14 @@ export class OSLib {
                 }
                 return isDir;
             },
-            path_isabs: (value) => {
-                return path.isAbsolute(read(this.instance, "str", value));
-            },
-            path_root: (value) => {
-                return write(
+            path_isabs: (value) =>
+                path.isAbsolute(read(this.instance, "str", value)),
+            path_root: (value) =>
+                write(
                     this.instance,
                     "str",
                     path.parse(read(this.instance, "str", value)).root,
-                );
-            },
+                ),
             path_ext: (value) =>
                 write(
                     this.instance,
@@ -142,6 +126,8 @@ export class OSLib {
             path_isabs: (value) => this.functions.path_isabs(value),
             path_root: (value) => this.functions.path_root(value),
             path_ext: (value) => this.functions.path_ext(value),
+            read_file: (value) => this.functions.read_file(value),
+            write_file: (a, b) => this.functions.write_file(a, b),
         };
     }
 }
