d76a4702fed0d8908a534da2fd45d5568f4f5806
梶塚太智 <kajizukataichi@outlook.jp>
2025-06-19 11:13:37 +0900

    Update stmt.rs

--- core/src/stmt.rs
+++ core/src/stmt.rs
@@ -94,31 +94,28 @@ impl Node for Stmt {
         } else if let Some(source) = source.strip_prefix("return ") {
             Some(Stmt::Return(Some(Expr::parse(source)?)))
         } else if let Some(after) = source.strip_prefix("load") {
-            /// Signature String: "fn1():ret1 as alias, fn2(arg:t):ret2, …" to
-            /// Vec<(Function Name, List of input and type, return type, alias)>
             pub fn parse_sigs(
                 sigs: &str,
             ) -> Option<Vec<(String, Vec<Type>, Type, Option<String>)>> {
                 let mut result = Vec::new();
                 for part in tokenize(sigs, &[","], false, true, false)? {
-                    // Separate alias: "... as alias"
                     let part = part.trim();
                     let (sig, alias) = part
                         .rsplit_once("as")
                         .map(|(sig, alias)| (sig, Some(alias.to_string())))
                         .unwrap_or((part, None));
-                    let Oper::Cast(Expr::Call(name, args), ret_ty) = Oper::parse(sig)? else {
+                    let Oper::Cast(Expr::Call(name, args), ret_typ) = Oper::parse(sig)? else {
                         return None;
                     };
-                    let mut args_ty = vec![];
+                    let mut args_typ = vec![];
                     for arg in args {
                         let Expr::Oper(arg) = arg else { return None };
-                        let Oper::Cast(_, arg_ty) = *arg.clone() else {
+                        let Oper::Cast(_, arg_typ) = *arg.clone() else {
                             return None;
                         };
-                        args_ty.push(arg_ty);
+                        args_typ.push(arg_typ);
                     }
-                    result.push((name, args_ty, ret_ty, alias));
+                    result.push((name, args_typ, ret_typ, alias));
                 }
                 Some(result)
             }
