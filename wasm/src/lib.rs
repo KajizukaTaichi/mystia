f56a66d25b29870dd9d6bd77ce259208b4f60717
梶塚太智 <kajizukataichi@outlook.jp>
2025-06-26 10:05:27 +0900

    Update lib.rs

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,4 +1,4 @@
-use mystia_core::{Compiler, type_to_json};
+use mystia_core::{Compiler, Type};
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
@@ -32,5 +32,35 @@ pub fn mystia(source: &str) -> Result<Mystia, String> {
     } else {
         let error_message = "failed to parse, compile or check type consistency";
         Err(compiler.occurred_error.unwrap_or(error_message.to_string()))
+    }
+}
+
+pub fn type_to_json(typ: &Type) -> String {
+    match typ {
+        Type::Integer => "\"int\"".to_string(),
+        Type::Number => "\"num\"".to_string(),
+        Type::Bool => "\"bool\"".to_string(),
+        Type::String => "\"str\"".to_string(),
+        Type::Void => "null".to_string(),
+        Type::Dict(dict) => format!(
+            "{{ type: \"dict\", fields: {{ {} }} }}",
+            dict.iter()
+                .map(|(k, (offset, typ))| format!(
+                    "{k}: {{ type: {}, offset: {offset} }}",
+                    type_to_json(typ)
+                ))
+                .collect::<Vec<_>>()
+                .join(", ")
+        ),
+        Type::Array(typ) => format!("{{ type: \"array\", element: {} }}", type_to_json(typ)),
+        Type::Enum(e) => format!(
+            "{{ type: \"enum\", enum: [{}] }}",
+            e.iter()
+                .map(|x| format!("\"{x}\""))
+                .collect::<Vec<_>>()
+                .join(", ")
+        ),
+        Type::Alias(name, _) => format!("{{ type: \"alias\", name: {name} }}"),
+        Type::Any => format!("\"any\""),
     }
 }
