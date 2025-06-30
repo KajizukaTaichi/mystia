64be68f4caa57057e42a6d6835a4b2e1ebf8ef1f
梶塚太智 <kajizukataichi@outlook.jp>
2025-06-30 15:20:57 +0900

    Update stmt.rs

--- core/src/stmt.rs
+++ core/src/stmt.rs
@@ -81,13 +81,11 @@ impl Node for Stmt {
             let Expr::Call(name, args) = Expr::parse(head)? else {
                 return None;
             };
-            let args = args
-                .iter()
-                .map(|x| {
-                    let Expr::Variable(x) = x else { return None };
-                    Some(x.clone())
-                })
-                .collect::<Option<Vec<_>>>()?;
+            let func = |x: &Expr| {
+                let Expr::Variable(x) = x else { return None };
+                Some(x.clone())
+            };
+            let args = args.iter().map(func).collect::<Option<Vec<_>>>()?;
             Some(Stmt::Macro(name, args, Expr::parse(value)?))
         } else if let Some(after) = source.strip_prefix("load") {
             let parse_sigs = |sigs: &str| {
