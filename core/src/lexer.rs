ea13c9165fbf69550302fc80e29f0d6534131133
梶塚太智 <kajizukataichi@outlook.jp>
2025-07-28 14:11:17 +0900

    Update expr.rs

--- core/src/lexer.rs
+++ core/src/lexer.rs
@@ -17,6 +17,14 @@ pub fn tokenize(
     let chars: Vec<String> = input.chars().map(String::from).collect();
     let mut index = 0;
 
+    fn include_letter(query: &str, chars: &Vec<String>, idx: usize) -> bool {
+        chars
+            .clone()
+            .get(idx..idx + query.chars().count())
+            .map(|i| query == i.concat())
+            .unwrap_or(false)
+    }
+
     while index < chars.len() {
         let c = chars.get(index)?.to_owned();
         if include_letter("~~", &chars, index) && !in_quote {
@@ -42,11 +50,7 @@ pub fn tokenize(
             index += 1;
         } else if [")", "]", "}"].contains(&c.as_str()) && !in_quote {
             current_token.push_str(c.as_str());
-            if let Some(i) = in_parentheses.checked_sub(1) {
-                in_parentheses = i;
-            } else {
-                return None;
-            }
+            in_parentheses = in_parentheses.checked_sub(1)?;
             index += 1;
         } else if c == "\"" {
             in_quote = !in_quote;
@@ -104,6 +108,85 @@ pub fn tokenize(
     }
     if !is_trim || (is_trim && !current_token.is_empty()) {
         tokens.push(current_token.clone());
+    }
+    Some(tokens)
+}
+
+pub fn is_identifier(name: &str) -> bool {
+    if name.is_empty() {
+        return false;
+    }
+    let mut chars = name.chars();
+    let first_char = chars.next().unwrap();
+    if !UnicodeXID::is_xid_start(first_char) {
+        return false;
+    }
+    if !chars.all(UnicodeXID::is_xid_continue) {
+        return false;
+    }
+    if RESERVED.contains(&name) {
+        return false;
+    }
+    if !name.is_ascii() {
+        return false;
+    }
+    true
+}
+
+pub fn str_format(input: &str) -> Option<Vec<String>> {
+    let mut tokens: Vec<String> = Vec::new();
+    let mut current_token = String::new();
+    let mut in_parentheses: usize = 0;
+    let mut is_escape = false;
+
+    for c in input.chars() {
+        if is_escape {
+            current_token.push(match c {
+                'n' => '\n',
+                't' => '\t',
+                'r' => '\r',
+                _ => c,
+            });
+            is_escape = false;
+        } else {
+            match c {
+                '{' => {
+                    if in_parentheses == 0 {
+                        if !current_token.is_empty() {
+                            tokens.push(current_token.clone());
+                        }
+                        current_token = c.to_string();
+                    } else {
+                        current_token.push(c)
+                    }
+                    in_parentheses += 1;
+                }
+                '}' => {
+                    current_token.push(c);
+                    in_parentheses = in_parentheses.checked_sub(1)?;
+                    if in_parentheses == 0 {
+                        if !current_token.is_empty() {
+                            tokens.push(current_token.clone());
+                        }
+                        current_token.clear();
+                    }
+                }
+                '\\' => {
+                    current_token.push(c);
+                    is_escape = true;
+                }
+                _ => current_token.push(c),
+            }
+        }
+    }
+
+    // Syntax error check
+    if is_escape || in_parentheses != 0 {
+        return None;
+    }
+    if !current_token.is_empty() {
+        tokens.push(current_token.clone());
+        current_token.clear();
     }
     Some(tokens)
 }
